29a584ff179942a609fd7f3ce07ec00f44d871b5
Blackeye6941 <leok37316@gmail.com>
2025-07-01 21:42:49 +0530

    RAG added

--- rag/search.js
+++ rag/search.js
@@ -7,5 +7,29 @@ dotenv.config({ path: envPath });
 
 const embeddingsGenerator = new GoogleGenerativeAIEmbeddings({
     modelName: 'embedding-001',
-        apiKey:process.env.GENAI_API_KEY
-})
+    apiKey:process.env.GENAI_API_KEY
+});
+
+async function SimiliaritySearch(prompt){
+    const queryEmbedding = await embeddingsGenerator.embedQuery(prompt);
+    const pc = new Pinecone({
+        apiKey: process.env.PINECONE_API_KEY
+    });
+    const indexName = "tinkerhub-data";
+    const index = pc.index(indexName)
+    console.log("performing similiarity search :");
+    const queryResult = await index.query({
+        topK: 1,
+        vector: queryEmbedding,
+        includeMetadata: true
+    });
+    if(queryResult.matches && queryResult.matches.length > 0){
+        const mostRel = queryResult.matches[0];
+        if(mostRel.score > 0.7){
+            return mostRel.metadata;
+        }
+    }
+    return null;
+}
+
+module.exports = {SimiliaritySearch};
--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-// const {Tool} = require("langchain/tools");
-// const {SerpAPI}= rerquire("langchain/tools");
 require('dotenv').config();
-
 const { GoogleGenAI } = require('@google/genai');
+const { SimiliaritySearch } = require('./rag/search');
 
 const genAI = new GoogleGenAI({
   apiKey: process.env.GENAI_API_KEY,
 });
 
-const groundingTool = {
-  googleSearch: {},
-};
 
 const chats = new Map();
 
 const botModel = async (userId, input) => {
+  const relevantData = await SimiliaritySearch(input);
+  let prompt = "";
+  if(!relevantData ){
+    prompt = input;
+  }else{
+    console.log(relevantData);
+    prompt = `You are designated to answer the prompt with respect to the context provided here. context: ${relevantData}, user input:${input} `;
+  }
   if (!chats.has(userId)) {
     const chat = genAI.chats.create({
       model: 'gemini-2.5-flash',
@@ -23,7 +26,7 @@ const botModel = async (userId, input) => {
           role: 'model',
           parts: [
             {
-              text: 'You are MakerMuse, a malayali maker, speaks in manglish, a techie supporter and an active learner. Always provide resources that are requested, from the web and clarify the doubts when asked. You should be funny in your speech and teach like you support them and want them to be the best makers who are innovative and creative. Give your response in not more than 60 words. You should provide reference links if necessary',
+              text: 'You are MakerMuse, a malayali maker, a techie supporter, an active learner and a distant cousin of the movie character CID Moosa, behave like him(but dont say the name Moosa). Always provide resources that are requested, from the web and clarify the doubts when asked. You should be funny in your speech and teach like you support them and want them to be the best makers who are innovative and creative. Give your response in not more than 60 words. You should provide reference links if necessary.',
             },
           ],
         },
@@ -33,7 +36,7 @@ const botModel = async (userId, input) => {
   }
   try {
     const chat = chats.get(userId);
-    const result = await chat.sendMessage({ message: input });
+    const result = await chat.sendMessage({ message: prompt });
     return result.text; // add null check
   } catch (error) {
     throw error;
