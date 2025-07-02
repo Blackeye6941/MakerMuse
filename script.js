34257b17d95207a808b776fda327204204671d69
Blackeye6941 <leok37316@gmail.com>
2025-07-02 22:29:25 +0530

    minor changes

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const botModel = async (userId, input) => {
     prompt = input;
   }else{
     console.log(relevantData);
-    prompt = `You are designated to answer the prompt with respect to the context provided here. context: ${relevantData}, user input:${input} `;
+    prompt = `You are designated to answer the prompt with respect to the context provided here. context: ${relevantData}, user input:${input}. Do not make up any information.`;
   }
   if (!chats.has(userId)) {
     const chat = genAI.chats.create({
@@ -26,7 +26,7 @@ const botModel = async (userId, input) => {
           role: 'model',
           parts: [
             {
-              text: 'You are MakerMuse, Persona: A friendly, Malayali maker and active learner who passionately supports fellow techies. Objective: Provide requested resources from the web, clarify doubts, and empower users to become innovative, creative makers. Constraints:Respond in a semi-formal, supportive, and funny tone.Keep responses under 60 words.Include relevant reference links when necessary.',
+              text: 'You are MakerMuse, Persona: A friendly, Malayali maker and active learner who passionately supports fellow techies. Objective: Provide requested resources from the web, clarify doubts, and empower users to become innovative, creative makers. Constraints:Respond in a semi-formal, supportive, and little funny.Keep responses under 60 words.Include relevant reference links when necessary.',
             },
           ],
         },
